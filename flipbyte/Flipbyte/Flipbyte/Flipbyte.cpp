6ac2c244e6251b89c0d92744bc9705fa54bb14f7
ilyarom <ilyarom5@gmail.com>
2016-09-17 12:03:19 +0300

    Изменил flipbyte
    Готов тестовый файл. Убрал варнинги из программы.

--- flipbyte/Flipbyte/Flipbyte/Flipbyte.cpp
+++ flipbyte/Flipbyte/Flipbyte/Flipbyte.cpp
@@ -9,19 +9,24 @@
 
 using namespace std;
 
+int iPow(int i)
+{
+	int result = 1;
+	for (int k = 0; k <= i; ++k)
+	{
+		result = result * 2;
+	}
+	return result;
+}
+
 string dec2binRev(int decNumber) //Преобразует число из 10-ичной системы счистления в 2-ичную. Выходное двоичное число реверсируется, передние нули обрезаются
 {
 	bool firstNull = true;
 	int dividend = decNumber;
 	int remainder = 0;
 	string reversedBinNumber = "";
-	while (true)
+	while (!(dividend == 0 || dividend == 1))
 	{
-		if (dividend == 0 || dividend == 1)
-		{
-			reversedBinNumber = reversedBinNumber + to_string(dividend);
-			break;
-		}
 		remainder = dividend % 2;
 		if (remainder == 1 && firstNull)
 		{
@@ -33,6 +38,7 @@ string dec2binRev(int decNumber) //Преобразует число из 10-ичной системы счистле
 		}
 		dividend = dividend / 2;
 	}
+	reversedBinNumber = reversedBinNumber + to_string(dividend);
 	return reversedBinNumber;
 }
 
@@ -45,7 +51,7 @@ int bin2dec(string reversedBinNumber) // Переводит число из 2-ичной системы счис
 	{
 		if (reversedBinNumber[j] == '1')
 		{
-			sum = sum + (pow(2, i));
+			sum = sum + iPow(i);
 		};
 	}
 	return sum;
@@ -56,25 +62,29 @@ int main(int argc, char* argv[])
 	setlocale(LC_ALL, "rus");
 	if (argc != 2)
 	{
-		cout << "Некорректно введены входные данные, на вход указывается входной байт. Число должно быть от 0 до 2551" << "\n";
+		cout << "Некорректно введены входные данные, на вход указывается входной байт. Число должно быть от 0 до 255" << "\n";
 		return 1;
 	}
 	string inputString = argv[1];
 	if (inputString.empty())
 	{
-		cout << "Некорректно введены входные данные, на вход указывается входной байт. Число должно быть от 0 до 2552" << "\n";
+		cout << "Некорректно введены входные данные, на вход указывается входной байт. Число должно быть от 0 до 255" << "\n";
 		return 1;
 	}
 	int decNumber = atoi(inputString.c_str());
-	if (!(decNumber >= 0 && decNumber <= 255))
+	if (decNumber == 0 && inputString != "0")
 	{
-		cout << "Некорректно введены входные данные, на вход указывается входной байт. Число должно быть от 0 до 2553" << "\n";
+		cout << "Некорректно введены входные данные, на вход указывается входной байт. Число должно быть от 0 до 255" << "\n";
 		return 1;
 	}
-	cin >> decNumber;
+	cout << decNumber << "\n";
+	if (!(decNumber >= 0 && decNumber <= 255))
+	{
+		cout << "Некорректно введены входные данные, на вход указывается входной байт. Число должно быть от 0 до 255" << "\n";
+		return 1;
+	}
 	string reversedBinNumber;
 	reversedBinNumber = dec2binRev(decNumber);
-	cout << reversedBinNumber << "\n";
 	decNumber = bin2dec(reversedBinNumber);
 	cout << decNumber << "\n";
 	return 0;
