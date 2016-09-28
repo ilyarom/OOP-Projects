a96557b2215b299886c58b188bab0fddd7772cb6
ilyarom <ilyarom5@gmail.com>
2016-09-28 09:33:09 +0300

    Доделал flipbyte и multimatrix
    Программы сделаны и корректно работают

--- flipbyte/Flipbyte/Flipbyte/Flipbyte.cpp
+++ flipbyte/Flipbyte/Flipbyte/Flipbyte.cpp
@@ -5,56 +5,35 @@
 #include <fstream>
 #include <string>
 #include <iostream>
-#include <cstdlib>
 
 using namespace std;
-
-int Pow(int i)
+ 
+size_t CountIterations(int number)
 {
-	int result = 1;
-	for (int k = 0; k <= i; ++k)
+	int i = 0;
+	while (number != 0)
 	{
-		result = result * 2;
+		number = number >> 1;
+		++i;
 	}
-	return result;
+	return i;
 }
 
-string Dec2binRev(int decNumber) //Преобразует число из 10-ичной системы счистления в 2-ичную. Выходное двоичное число реверсируется, передние нули обрезаются
+int Reverse(size_t i, int number)
 {
-	bool firstNull = true;
-	int dividend = decNumber;
-	int remainder = 0;
-	string reversedBinNumber = "";
-	while (!(dividend == 0 || dividend == 1))
+	int j = 7;
+	int resNumber = 0;
+	while (i > 0)
 	{
-		remainder = dividend % 2;
-		if (remainder == 1 && firstNull)
+		if (number % 2 == 1)
 		{
-			firstNull = false;
+			resNumber = resNumber + (1 << j);
 		}
-		if (!firstNull)
-		{
-			reversedBinNumber = reversedBinNumber + to_string(remainder);
-		}
-		dividend = dividend / 2;
+		number = (number >> 1);
+		--i;
+		--j;
 	}
-	reversedBinNumber = reversedBinNumber + to_string(dividend);
-	return reversedBinNumber;
-}
-
-int Bin2dec(string reversedBinNumber) // Переводит число из 2-ичной системы счистления в 10-ичную
-{
-	size_t maxDegree = reversedBinNumber.length() - 1;
-	int sum = 0;
-	int j = 0;
-	for (int i = maxDegree; i >= 0; --i)
-	{
-		if (reversedBinNumber[j] == '1')
-		{
-			sum = sum + Pow(i);
-		};
-	}
-	return sum;
+	return resNumber;
 }
 
 int main(int argc, char* argv[])
@@ -62,31 +41,22 @@ int main(int argc, char* argv[])
 	setlocale(LC_ALL, "rus");
 	if (argc != 2)
 	{
-		cout << "Некорректно введены входные данные, на вход указывается входной байт. Число должно быть от 0 до 255" << "\n";
+		cout << "Некорректно введены входные данные, на вход указывается входной байт" << "\n";
 		return 1;
 	}
 	string inputString = argv[1];
 	if (inputString.empty())
 	{
-		cout << "Некорректно введены входные данные, на вход указывается входной байт. Число должно быть от 0 до 255" << "\n";
+		cout << "Пустой ввод. На вход должен указываться входной байт" << "\n";
 		return 1;
 	}
-	int decNumber = atoi(inputString.c_str());
-	if (decNumber == 0 && inputString != "0")
+	int number = atoi(inputString.c_str());
+	if (!(number >= 0 && number <= 255) || (inputString != "0" && number == 0))
 	{
-		cout << "Некорректно введены входные данные, на вход указывается входной байт. Число должно быть от 0 до 255" << "\n";
+		cout << "Входные данные должны быть числом от 0 до 255" << "\n";
 		return 1;
 	}
-	if (!(decNumber >= 0 && decNumber <= 255))
-	{
-		cout << "Некорректно введены входные данные, на вход указывается входной байт. Число должно быть от 0 до 255" << "\n";
-		return 1;
-	}
-	string reversedBinNumber;
-	reversedBinNumber = Dec2binRev(decNumber);
-	decNumber = Bin2dec(reversedBinNumber);
-	cout << decNumber << "\n";
+	int i = CountIterations(number);
+	cout << Reverse(i, number) << "\n";
 	return 0;
 }
-
-
