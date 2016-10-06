f77ebfeda1054c3acbce56609f8cbcc32b6cbf7e
ilyarom <ilyarom5@gmail.com>
2016-10-06 12:49:40 +0300

    Добавил PrimeNumber
    Добавил программу поиска простых чисел методом Решета Эратосфена
    (Без set)

---
+++ lab02/PrimeNumber/PrimeNumber/PrimeNumber/PrimeNumber.cpp
@@ -0,0 +1,50 @@
+// PrimeNumber.cpp: определ€ет точку входа дл€ консольного приложени€.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <set>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	size_t n;
+	cin >> n;
+	if (n > 100000000)
+	{
+		cout << "”казанное число не должно превышать 100 миллионов." << "\n";
+		return 1;
+	}
+	vector<bool> numbers(n + 1, true);
+	numbers[1] = false;
+	for (size_t i = 2; ((i*i) <= n); ++i)
+	{
+		if (numbers[i])
+		{
+			for (size_t j = (i*i); j <= n; j += i)
+			{
+				if (numbers[j])
+				{
+					numbers[j] = false;
+				}
+			}
+				
+		}
+	}
+	for (size_t i = 1; i <= n; ++i)
+	{
+		if (numbers[i])
+		{
+			cout << i << " ";
+			if (i % 10 == 0)
+			{
+				cout << "\n";
+			}
+		}
+	}
+
+    return 0;
+}
+
